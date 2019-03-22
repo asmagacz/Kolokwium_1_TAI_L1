f7298a76efabcb6f3d0020f7e0cee3260d1d270a
asmagacz <29546@student.pwsztar.edu.pl>
2019-03-22 15:50:43 +0100

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,9 @@
+let nav = '<h1>Nawigacja</h1>' +'<a class="nav-link" href="kontakt.html">Kontakt </a>'
+            +'<a class="nav-link" href="index.html"> Index</a>';
+
+
+let elem = document.querySelector(".nav-bar");
+elem.innerHTML = nav;
+
+
+
